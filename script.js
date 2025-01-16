510aa98ebd1a962423661384469c8c85826ffc85
Jagdish Padhi <jagdishpadhi9@gmail.com>
2025-01-16 20:28:54 +0530

    updated

--- script.js
+++ script.js
@@ -1,4 +1,27 @@
+// Hamburger Menu Toggle
+const hamburger = document.getElementById("hamburger");
+const navLinks = document.querySelector(".nav-links");
 
+hamburger.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+});
 
- 
-  
+let changingWord = document.querySelector(".typing-word");
+
+let wordsArray = [
+  "Programmer",
+  "Problem Solver",
+  "Lifelong learner",
+];
+
+const wordChanger = async () => {
+  for (i = 0; i < wordsArray.length; i++) {
+    changingWord.innerText = wordsArray[i];
+
+    await new Promise(resolve => setTimeout(resolve, 5000));
+  }
+
+  wordChanger()
+};
+
+wordChanger();
